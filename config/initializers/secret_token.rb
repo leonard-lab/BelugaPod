99fdbeb444362c63f8b7da570535c0fe6aba72f7
Dan Swain <dswain@princeton.edu>
2011-11-13 19:59:09 -0500

    Initial commit (some work done already).

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WhalesOnRails::Application.config.secret_token = '9504fd470dc2420c19e403b4f1a74116f5c89948aaa41734f54b9970c65271138814a5b8d9994a4135be591b0cdcb45764930d57640bfbbdb40208c2e9cf44b1'
