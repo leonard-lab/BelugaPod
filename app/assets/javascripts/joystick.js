a207893c7ce73d6dedb7d748173e46c1a4d7ec47
Dan Swain <dswain@princeton.edu>
2011-11-14 11:44:05 -0500

    Rough working UI

--- app/assets/javascripts/joystick.js
+++ app/assets/javascripts/joystick.js
@@ -1,16 +1,45 @@
-$(document).ready(function(){
-    $("#EnableXY").button();
+g_Enabled = false;
 
-    $("#green_dot").show();
-    moveGreenDotTo(0, 0);
+$(document).ready(function(){
+    $("#Zero").button();
+
+    $("#Enable").button().toggle(function() {
+        $(this).button('option', 'label', 'Disable');
+        g_Enabled = true;
+    }, function() {
+        moveGreenDotTo(0, 0);
+        moveRedDotTo(0);
+        $(this).button('option', 'label', 'Enable');
+        g_Enabled = false;
+    });
+    
+    $("#Zero").click(function(){
+        moveGreenDotTo(0, 0);
+        moveRedDotTo(0);
+    });
 
     $("#XY").click(function(ev){
-        var X = ev.pageX;
-        var Y = ev.pageY;
-        X -= $("#XY").position().left + 0.5*$("#XY").width() - 7.5;
-        Y -= $("#XY").position().top + 0.5*$("#XY").height() + 7.5;
-        moveGreenDotTo(X, Y);
+        if(g_Enabled)
+        {
+            var X = ev.pageX;
+            var Y = ev.pageY;
+            X -= $("#XY").position().left + 0.5*$("#XY").width() - 7.5;
+            Y -= $("#XY").position().top + 0.5*$("#XY").height() + 7.5;
+            moveGreenDotTo(X, Y);
+        }
     });
+
+    $("#Z").click(function(ev){
+        if(g_Enabled)
+        {
+            var Y = ev.pageY;
+            Y -= $("#Z").position().top + 0.5*$("#Z").height() + 7.5;
+            moveRedDotTo(Y);
+        }
+    });
+
+    moveGreenDotTo(0, 0);
+    moveRedDotTo(0);
     
  });
 
@@ -25,6 +54,37 @@ function moveGreenDotTo(x, y)
         at: "center center",
         of: "#XY",
         offset: off
-    });        
+    });
+
+    $("#Xlabel").text(x);
+    $("#Ylabel").text(y);    
 }
 
+function moveRedDotTo(y)
+{
+    var w = 15;
+    var px = -0.5*$("#Z").width() + 0.5*w;
+    var py = y;
+    var off = px + " " + py;
+    $("#red_dot").position({
+        my: "center center",
+        at: "center center",
+        of: "#Z",
+        offset: off
+    });
+
+    $("#Zlabel").text(y);
+}
+
+function toggleEnabled()
+{
+    g_Enabled = !g_Enabled;
+    if(g_Enabled == true)
+    {
+        $("#Enable .ui-button-text").text("Disable").button("refresh");
+    }
+    else
+    {
+        $("#Enable .ui-button-text").text("Enable").button("refresh");
+    }
+}
