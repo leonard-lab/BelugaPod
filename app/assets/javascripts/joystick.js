e8a397539ed1ea07519d5ea8dfaa6c1455403b46
Dan Swain <dswain@princeton.edu>
2011-11-15 13:08:19 -0500

    Tidy up joystick page.  Position is still a little off, but better.  Could use more tweaking.

--- app/assets/javascripts/joystick.js
+++ app/assets/javascripts/joystick.js
@@ -1,5 +1,9 @@
 var g_Enabled = false;
 
+var g_HaveCounter;
+var g_NeedSend = false;
+var g_SendPeriod = 100;  // milliseconds between updates
+
 $(document).ready(function(){
     $("#Zero").button();
 
@@ -34,18 +38,32 @@ $(document).ready(function(){
     $("#Z").unbind("click").click(function(ev){
         if(g_Enabled)
         {
-            updateKinematicsZ(ev.pageY - $("#Z").position().top + 0.5*$("#Z").height());
+            updateKinematicsZ(ev.pageY - $("#Z").position().top - 0.5*$("#Z").height());
         }
     });
 
     movePuckTo(0, 0);
     moveVSliderTo(0);
+
+    if(g_HaveCounter == undefined)
+    {
+        window.setInterval(doSend, g_SendPeriod);
+        g_HaveCounter = true;
+    }
     
  });
 
+function doSend()
+{
+    if(g_NeedSend)
+    {
+        $("form#new_kinematic").submit();
+        g_NeedSend = false;
+    }
+}
+
 function enableDraggables()
 {
-    debugOut("Hi");
     $("#puck").draggable({
         containment: "#XY",
         drag: function(){
@@ -94,20 +112,20 @@ function debugOut(txt)
 function updateKinematicsXY(X, Y)
 {
     movePuckTo(X, Y);
-    $("form#new_kinematic").submit();                
+    g_NeedSend = true;
 }
 
 function updateKinematicsZ(Y)
 {
     moveVSliderTo(Y);
-    $("form#new_kinematic").submit();                
+    g_NeedSend = true;
 }
 
 function movePuckTo(x, y)
 {
-    var w = 15;
+    var w = 30;
     var px = x - 0.5*w;
-    var py = y;
+    var py = y - 0.5*w;
     var off = px + " " + py;
     $("#puck").position({
         my: "center center",
@@ -137,9 +155,9 @@ function movePuckTo(x, y)
 
 function moveVSliderTo(y)
 {
-    var w = 15;
-    var px = 1; //-0.5*$("#Z").width() + 0.5*w;
-    var py = y;
+    var h = 10;
+    var px = 1;
+    var py = y - 0.5*h;
     var off = px + " " + py;
     $("#vslider").position({
         my: "left center",
