5f74b9540831a5c839e09afb5352aa73e04025cf
Dan Swain <dswain@princeton.edu>
2012-03-06 18:04:45 -0500

    Tests passing!

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -5,27 +5,27 @@ describe PagesController do
 
   describe "Get home" do
     it "should show a warning if the IPC server is not running" do
-      get :home
-      response.should have_selector("div.warning", :content => "server")
+      get '/'
+      last_response.body.should have_selector("div.warning", :content => "server")
     end
 
     it "should not show a warning if the IPC server is running" do
       BelugaSocket.should_receive(:okay?).and_return(true)
-      get :home
-      response.should_not have_selector("div.warning", :content => "server")
+      get '/'
+      last_response.body.should_not have_selector("div.warning", :content => "server")
     end
   end
 
   describe "get ipc" do
     it "should show a warning if the IPC server is not running" do
-      get :ipc
-      response.should have_selector("div#server_status", :content => "not running")
+      get '/ipc'
+      last_response.body.should have_selector("div#server_status", :content => "not running")
     end
 
     it "should tell us if the IPC server is running" do
       BelugaSocket.should_receive(:okay?).and_return(true)
-      get :ipc
-      response.should have_selector("div#server_status", :content => "up and running")
+      get '/ipc'
+      last_response.body.should have_selector("div#server_status", :content => "up and running")
     end
   end
 end
--- spec/controllers/positions_controller_spec.rb
+++ spec/controllers/positions_controller_spec.rb
@@ -18,22 +18,22 @@ describe PositionsController do
     end
 
     it "should respond with the position if the short param is set" do
-      post :create, :position => @attr, :short => "1"
-      last_response.body.should contain [@attr[:x], @attr[:y], @attr[:z]].join(" ")
+      post '/positions', :position => @attr, :short => "1"
+      last_response.body.should include [@attr[:x], @attr[:y], @attr[:z]].join(" ")
     end
   end
 
   describe "get index" do
     it "should respond with all positions when the short param is set" do
-      get :index, :short => "1"
-      last_response.body.should contain(("0.0 "*9).strip)
+      get '/positions', :short => "1"
+      last_response.body.should include(("0.0 "*9).strip)
     end
   end
 
   describe "get show" do
     it "should respond with the position if the short param is set" do
-      get :show, :id => 0, :short => "1"
-      last_response.body.should contain "0.0 0.0 0.0"
+      get '/positions/', :id => 0, :short => "1"
+      last_response.body.should include "0.0 0.0 0.0"
     end
   end
 
